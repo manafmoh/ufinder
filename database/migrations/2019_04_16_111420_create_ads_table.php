1bc7b8b8b08afbb07dbea1f5200b647a6342dbc9
Manaf <manafmoh@gmail.com>
2019-04-16 17:51:13 +0530

    initial commit

---
+++ database/migrations/2019_04_16_111420_create_ads_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->boolean('featured');
+            $table->float('amount');
+            $table->longText('image');
+            $table->text('body');
+            $table->enum('post_type', ['sell', 'rent']);
+            $table->enum('type', ['new', 'resale']);
+            $table->char('country', 2);
+            $table->string('city');
+            $table->string('area');
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}
