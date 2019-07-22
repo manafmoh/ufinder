97ef50a4453fc07bcd69e6e3565bc454d7801f46
Manaf <manafmoh@gmail.com>
2019-07-22 15:58:56 +0530

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Contracts\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,8 +24,11 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
 
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
+        if (\App::environment() === 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
