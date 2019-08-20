dea7f3be0c2ec738a953eeb7c8261255aae15f03
Manaf <manafmoh@gmail.com>
2019-08-20 12:22:52 +0530

    Google Credetial file

--- resources/js/Helpers/facebook.js
+++ resources/js/Helpers/facebook.js
@@ -2,7 +2,8 @@ import Token from './Token';
 import AppStorage from './AppStorage';
 class Facebook {
     login(data){
-        axios.post('https://ufindna.com/api/auth/facebooklogin', null,{ params: { 'id':data.id , 'name':data.name, 'firstname':data.firstname, 'lastname':data.lastname,'email':data.email} })
+        //https://ufindna.com - i removed, pls add and check
+        axios.post('/api/auth/facebooklogin', null,{ params: { 'id':data.id , 'name':data.name, 'firstname':data.firstname, 'lastname':data.lastname,'email':data.email} })
            .then(res => this.responseAfterLogin(res))
            .catch(error => {
                console.log(error.response.data);
--- resources/js/Helpers/google.js
+++ resources/js/Helpers/google.js
@@ -2,7 +2,7 @@ import Token from './Token';
 import AppStorage from './AppStorage';
 class Google {
     login(data){
-        axios.post('http://ufindna.com/api/auth/googlelogin', null,{ params: { 'id':data.id , 'name':data.name, 'firstname':data.firstname, 'lastname':data.lastname,'email':data.email} })
+        axios.post('/api/auth/googlelogin', null,{ params: { 'id':data.id , 'name':data.name, 'firstname':data.firstname, 'lastname':data.lastname,'email':data.email} })
            .then(res => this.responseAfterLogin(res))
            .catch(error => {
                console.log(error.response.data);
@@ -12,6 +12,7 @@ class Google {
     responseAfterLogin(res) {
         const access_token = res.data.access_token;
         const user = res.data.user;
+        console.log('AF', user);
         if(Token.isValidGoogle(access_token)){
             AppStorage.store(access_token, user);
             window.location = "/";
