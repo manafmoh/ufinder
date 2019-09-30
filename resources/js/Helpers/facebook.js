3bf0cf3c675f6cb2a99a24908affc4712f803f5c
Manaf <manafmoh@gmail.com>
2019-09-30 12:26:49 +0530

    Login message

--- resources/js/Helpers/facebook.js
+++ resources/js/Helpers/facebook.js
@@ -1,12 +1,14 @@
 import Token from './Token';
 import AppStorage from './AppStorage';
+import swal from 'sweetalert';
 class Facebook {
     login(data){
         //https://ufindna.com - i removed, pls add and check
         axios.post('/api/auth/facebooklogin', null,{ params: { 'id':data.id , 'name':data.name, 'firstname':data.firstname, 'lastname':data.lastname,'email':data.email} })
            .then(res => this.responseAfterLogin(res))
            .catch(error => {
-               console.log(error.response.data);
+               //console.log(error.response.data);
+               swal('Sorry!!', error.response.data, "error");
                EventBus.$emit('isFacebookLoggin', false);
             });
     }
@@ -15,7 +17,7 @@ class Facebook {
         const user = res.data.user;
         if(Token.isValidFacebook(access_token)){
             AppStorage.store(access_token, user);
-            window.location = "/";
+            window.location = "/post";
         }
     }
 
--- resources/js/Helpers/google.js
+++ resources/js/Helpers/google.js
@@ -1,11 +1,13 @@
 import Token from './Token';
 import AppStorage from './AppStorage';
+import swal from 'sweetalert';
 class Google {
     login(data){
         axios.post('/api/auth/googlelogin',null, { params: { 'id':data.id , 'name':data.name, 'firstname':data.firstname, 'lastname':data.lastname,'email':data.email} })
            .then(res => this.responseAfterLogin(res))
            .catch(error => {
-               console.log(error.response.data);
+               //console.log(error.response.data);
+               swal('Sorry!!', error.response.data, "error");
               // EventBus.$emit('isGoogleLoggin', false);
             });
     }
@@ -15,7 +17,7 @@ class Google {
         //console.log('AF', user);
         if(Token.isValidGoogle(access_token)){
             AppStorage.store(access_token, user);
-            window.location = "/";
+            window.location = "/post";
         }
     }
 
